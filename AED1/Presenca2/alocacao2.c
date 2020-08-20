971d809beda04d43094db2abd3ef69b39403868c
Heitor Freitas <heitor.ff@ufu.br>
2020-08-20 10:45:26 -0300

    Adiconando testes com vogais maiusculas

--- AED1/Presenca2/alocacao2.c
+++ AED1/Presenca2/alocacao2.c
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
   scanf("%[^\n]s", str);
   for (size_t i = 0; i < count; i++)
   {
-    if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u')
+    if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' && str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U')
     {
       aux[j] = str[i];
       j++;
