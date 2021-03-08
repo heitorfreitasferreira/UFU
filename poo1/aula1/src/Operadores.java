50db92ea8c865b85ed0178d2bcfeca18a263ae42
heitorfreitasferreira <heitor.ff@ufu.br>
2021-03-08 20:34:05 -0300

    aula 3 poo, pratica java

---
+++ poo1/aula1/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args){
+        int op1, op2;
+        float result;
+        op1 = 10;
+        op2 = 3;
+        result = (float)op1 / op2;
+        int vet[] = new int[10];
+        int mat[][] = new  int [3][3];
+        for (int i =0; i< vet.length;i++){
+            System.out.println(vet[i]);
+        }
+        System.out.println("Resultado: "+ result);
+    }
+}
