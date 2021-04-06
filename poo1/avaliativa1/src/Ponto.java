4a363d0810b2cdb12b24eee571b4a5230422c4b1
heitorfreitasferreira <heitor.ff@ufu.br>
2021-04-06 12:31:45 -0300

    aula java mais trab

---
+++ poo1/avaliativa1/src/Ponto.java
@@ -0,0 +1,23 @@
+public class Ponto {
+    import
+    private double x,y;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double distancia(Ponto outro){
+        return
+    }
+}
