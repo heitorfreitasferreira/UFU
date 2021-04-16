8cd76b9580b7c4c2d9f070788ae903a255510e5d
heitorfreitasferreira <heitor.ff@ufu.br>
2021-04-16 13:01:10 -0300

    tentando fazer o teste de est por brute force

---
+++ est/tentativa.lua
@@ -0,0 +1,13 @@
+local x1 = 40
+local x2 = 25
+local l1 = 40
+local l2 = 25
+local e = 2.7
+
+print("f("..x1..") = " .. (l1*e)^(l1*x1*(-1)))
+
+print("f("..x2..") = " .. (l1*e)^(l1*x2*(-1)))
+
+print("f("..x1..") = " .. (l2*e)^(l2*x1*(-1)))
+
+print("f("..x2..") = " .. (l2*e)^(l2*x2*(-1)))
