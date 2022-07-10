59c5df0de902eb9f5ca5d205ca9de4f31caa2f20
Heitor Freitas <heitor.ff@ufu.br>
2022-07-09 21:42:34 -0300

    Piggy back

--- aa/Par Mais Proximo/src/PMP.java
+++ aa/Par Mais Proximo/src/PMP.java
@@ -7,22 +7,23 @@ public class PMP {
     public static PMP getInstance(){
         return instance;
     }
+    //#region Estratégia dividir e conquistar
     private static Tupla<Double, Tupla<Point,Point>> divideCoquerBase(ArrayList<Point> pontos, int start, int end){
-    double dist = pontos.get(start).distance(pontos.get(start+1));
-        Tupla<Double,Tupla<Point,Point>> ret = new Tupla<>(dist, new Tupla<>(pontos.get(start), pontos.get(start+1)));
-        for(int i = start; i < end-1; i++){
-            for(int j = i+1; j < end; j++){
-                double aux = pontos.get(i).distance(pontos.get(j));
-                if(aux < dist){
-                    dist = aux;
-                    ret.first = aux;
-                    ret.second.first = pontos.get(i);
-                    ret.second.second = pontos.get(j);
+        double dist = pontos.get(start).distance(pontos.get(start+1));
+            Tupla<Double,Tupla<Point,Point>> ret = new Tupla<>(dist, new Tupla<>(pontos.get(start), pontos.get(start+1)));
+            for(int i = start; i < end-1; i++){
+                for(int j = i+1; j < end; j++){
+                    double aux = pontos.get(i).distance(pontos.get(j));
+                    if(aux < dist){
+                        dist = aux;
+                        ret.first = aux;
+                        ret.second.first = pontos.get(i);
+                        ret.second.second = pontos.get(j);
+                    }
                 }
             }
+            return ret;
         }
-        return ret;
-    }
     public static Tupla<Double, Tupla<Point,Point>> divideCoquer(ArrayList<Point> pontos, int start, int end){
         if(end-start < 100) return divideCoquerBase(pontos, start, end);
         int mid = (start+end)/2;
@@ -58,4 +59,83 @@ public class PMP {
         }
         return ret;
     }
+    //#endregion Dividir e conquistar
+
+    //#region Piggybacking 
+    private static ArrayList<Point> mergePiggyBackArrayList(ArrayList<Point> list1, ArrayList<Point> list2){
+        ArrayList<Point> ret = new ArrayList<>();
+        int i = 0, j = 0;
+        while(i < list1.size() && j < list2.size()){
+            if(list1.get(i).getY() < list1.get(i).getY()){
+                ret.add(list1.get(i));
+                i++;
+            }else{
+                ret.add(list2.get(j));
+                j++;
+            }
+        }
+        while(i < list1.size()){
+            ret.add(list1.get(i));
+            i++;
+        }
+        while(j < list2.size()){
+            ret.add(list2.get(j));
+            j++;
+        }
+        return ret;
+    }
+ 
+    private static Tripla<Double, Tupla<Point,Point>,ArrayList<Point>> divideCoquerPiggyBackBase(ArrayList<Point> pontos, int start, int end){
+        double dist = pontos.get(start).distance(pontos.get(start+1));
+            Tripla<Double,Tupla<Point,Point>,ArrayList<Point>> ret = new Tripla<>(dist, new Tupla<>(pontos.get(start), pontos.get(start+1)),pontos);
+            for(int i = start; i < end-1; i++){
+                for(int j = i+1; j < end; j++){
+                    double aux = pontos.get(i).distance(pontos.get(j));
+                    if(aux < dist){
+                        dist = aux;
+                        ret.first = aux;
+                        ret.second.first = pontos.get(i);
+                        ret.second.second = pontos.get(j);
+                    }
+                }
+            }
+            ret.getSortedArray().sort((a,b)-> a.y.compareTo(b.y));
+            return ret;
+        }
+    
+    private static Tripla<Double, Tupla<Point,Point>,ArrayList<Point> >divideCoquerPiggyBackHidden(ArrayList<Point> pontos, int start, int end){
+        if(end-start < 100) return divideCoquerPiggyBackBase(pontos, start, end);
+        int mid = (start+end)/2;
+        Tripla<Double,Tupla<Point,Point>,ArrayList<Point>> left = divideCoquerPiggyBackBase(pontos, start, mid);
+        Tripla<Double,Tupla<Point,Point>,ArrayList<Point>> right = divideCoquerPiggyBackBase(pontos, mid, end);
+        Tripla<Double,Tupla<Point,Point>,ArrayList<Point>> ret = new Tripla<>(left.first < right.first ? left.first : right.first, new Tupla<>(left.second.first, right.second.first),mergePiggyBackArrayList(left.getSortedArray(),right.getSortedArray()));
+        if(left.first < right.first){
+            ret.first = left.first;
+            ret.second = left.second;
+        }else{
+            ret.first = right.first;
+            ret.second = right.second;
+        }
+        
+        for (int i = 0; i < ret.getSortedArray().size(); i++) {
+            for (int j = 0; j < 8; j++) {
+                if(i+j>ret.getSortedArray().size()){
+                    Double dAux = pontos.get(i).distance(ret.getSortedArray().get(i+j));
+                    if(dAux < ret.first){
+                        ret.first = dAux;
+                        ret.second.first = ret.getSortedArray().get(i);
+                        ret.second.second = ret.getSortedArray().get(i+j);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+    public static Tupla<Double, Tupla<Point,Point>> divideCoquerPiggyBack(ArrayList<Point> param){
+        Tripla<Double, Tupla<Point,Point>,ArrayList<Point>> ret = divideCoquerPiggyBackHidden(param, 0, param.size()-1);
+        return new Tupla<>(ret.getDistance(), ret.getPoints());
+    }
+    
+        //#endregion Piggybacking
+
 }
--- aa/Par Mais Proximo/src/Tripla.java
+++ aa/Par Mais Proximo/src/Tripla.java
@@ -7,4 +7,23 @@ public class Tripla<Type1,Type2,Type3> {
         this.second = second;
         this.third = third;
     }
+    public Type1 getDistance() {
+        return first;
+    }
+    public Type2 getPoints() {
+        return second;
+    }
+    public Type3 getSortedArray() {
+        return third;
+    }
+    public void setDistance(Type1 value) {
+        this.first = value;
+    }
+    public void setPoints(Type2 value) {
+        this.second = value;
+    }
+    public void setSortedArray(Type3 value) {
+        this.third = value;
+    }
+
 }
