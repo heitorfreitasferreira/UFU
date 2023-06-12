aefc8d4aacf9b0ca9add0284223fc0faf5cb81b1
Heitor Freitas Ferreira <heitor.ff@ufu.br>
2023-06-12 19:04:46 -0300

    funcionalização cco

--- cco/integral.py
+++ cco/integral.py
@@ -5,7 +5,7 @@ def aproximar_integral(
     funcao: callable,
     intervalo: tuple[float, float],
     num_trapezios: int = 1000
-):
+) -> float:
 
     inicio_intervalo = min(intervalo)
     fim_intervalo = max(intervalo)
@@ -28,14 +28,13 @@ def ler_lambda() -> callable:
 
 def main():
     f = ler_lambda()
-    a = float(input("Digite INICIO o do intervalo: "))
-    b = float(input("Digite FIM o do intervalo: "))
+    a = float(eval(input("Digite INICIO o do intervalo: ")))
+    b = float(eval(input("Digite FIM o do intervalo: ")))
 
-    integral_aproximada = aproximar_integral(
+    print("Integral aproximada:", aproximar_integral(
         funcao=f,
         intervalo=(a, b)
-    )
-    print("Integral aproximada:", integral_aproximada)
+    ))
 
 
 if __name__ == "__main__":
