dccbc1f956fc03cb5ebd729def7fdd1641f731d1
heitorfreitasferreira <heitor.ff@ufu.br>
2024-02-09 14:49:54 -0300

    busca

---
+++ ia/search/graph.py
@@ -0,0 +1,238 @@
+"""Módulo com a implementação do grafo e do algoritmo A* para encontrar o menor 
+caminho entre duas cidades\n
+
+Raises:
+    KeyError: Tenta acessar um vértice que não existe
+    KeyError: Tentativa de acessar um vértice que não existe pelo nome
+    ValueError: Tentativa de acessar um vértice com um tipo de dado inválido
+"""
+from typing import List, Set, Dict, Tuple, Optional
+
+
+class Vertex:
+    """Classe que representa um vértice do grafo
+    """
+
+    def __init__(self, vertex_id: int, name: str, lat: float, long: float):
+        """Construtor da classe Vertex
+
+        Args:
+            vertex_id (int): identificador ÚNICO do vértice, fica a 
+            resposabilidade do usuário garantir a unicidade
+            name (str): Nome do vértice, normalmente cidade
+            lat (float): Latitude da cidade para ser usado na heurística do 
+            algoritmo A*
+            long (float): Longitude da cidade para ser usado na heurística do 
+            algoritmo A*
+        """
+        self.id = vertex_id
+        self.name = name
+        self.lat = lat
+        self.long = long
+        self.neighbors: List[Vertex] = []
+
+    def add_neighbor(self, neighbor: 'Vertex') -> None:
+        """Adiciona um vizinho ao vértice
+
+        Args:
+            neighbor (Vertex): Vizinho a ser adicionado
+        """
+        if neighbor not in self.neighbors:
+            self.neighbors.append(neighbor)
+
+    def get_euclidian_distance(self, vertex: 'Vertex') -> float:
+        """Calcula a distância euclidiana entre dois vértices
+
+        Args:
+            vertex (Vertex): Vértice para calcular a distância
+
+        Returns:
+            float: distância euclidiana entre os vértices
+        """
+        return ((self.lat - vertex.lat) ** 2 + (self.long - vertex.long) ** 2) ** 0.5
+
+    def __str__(self):
+        return self.name
+
+
+class Graph:
+    """Classe que representa um grafo não direcionado
+    """
+
+    def __init__(self):
+        self.vertices: Dict[int, Vertex] = {}
+
+    def add_vertex(self, vertex: Vertex):
+        """Adiciona um vértice ao grafo
+
+        Args:
+            vertex (Vertex): Vértice a ser adicionado
+
+        Returns:
+            boolean: True se o vértice foi adicionado, False caso contrário
+        """
+        if isinstance(vertex, Vertex) and vertex.id not in self.vertices:
+            self.vertices[vertex.id] = vertex
+            return True
+        return False
+
+    def add_edge(self, v1: int, v2: int):
+        """Adiciona uma aresta entre dois vértices consideraqndo que o grafo é não direcionado
+
+        Args:
+            v1 (int): identificador do vértice 1
+            v2 (int): identificador do vértice 2
+
+        Returns:
+            boolean: True se a aresta foi adicionada, False caso contrário
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add_neighbor(self.vertices[v2])
+            self.vertices[v2].add_neighbor(self.vertices[v1])
+            return True
+        return False
+
+    def get_vertices(self):
+        """Retorna os vértices do grafo
+
+        Returns:
+            List[Vertex]: Lista de vértices do grafo
+        """
+        return self.vertices.keys()
+
+    def get_vertex_by_id(self, vertex_id: int) -> Vertex:
+        """Retorna um vértice pelo identificador
+
+        Args:
+            vertex_id (int): identificador do vértice
+
+        Raises:
+            KeyError: Não encontrou o vértice
+
+        Returns:
+            Vertex: Vértice encontrado
+        """
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        raise KeyError(f"Vertex {vertex_id} not found")
+
+    def get_vertex_by_name(self, name: str) -> Vertex:
+        """Retorna um vértice pelo nome, preferir usar a busca pelo 
+        identificador que é mais performática
+
+        Args:
+            name (str): nome do vértice
+
+        Raises:
+            KeyError: Não encontrou o vértice
+
+        Returns:
+            Vertex: Vértice encontrado
+        """
+        for vertex in self.vertices.values():
+            if vertex.name == name:
+                return vertex
+        raise KeyError(f"Vertex {name} not found")
+
+    def get_vertex(self, key) -> Vertex:
+        """Retorna um vértice pelo identificador ou pelo nome
+
+        Args:
+            key (str|int): identificador ou nome do vértice
+
+        Raises:
+            ValueError: Tipo de busca não suportada
+
+        Returns:
+            Vertex: Vértice encontrado
+        """
+        if isinstance(key, int):
+            return self.get_vertex_by_id(key)
+        if isinstance(key, str):
+            return self.get_vertex_by_name(key)
+        raise ValueError("Key must be an integer or a string")
+
+    def a_star(self, start_id: int, end_id: int, view_trace=False):
+        """Executa o algoritmo A* para encontrar o menor caminho entre dois vértices
+
+        Args:
+            start_id (int): identificador do vértice de origem
+            end_id (int): identificador do vértice de destino
+            view_trace (bool, optional): Flag indicando se deverá imprimir na 
+            tela o caminho percorrido pelo algoritmo até encontrar o menor 
+            caminho. Defaults to False.
+
+        Returns:
+            List[Vertex]: Menor caminho entre os vértices de origem e destino
+        """
+        if start_id == end_id:
+            return [self.get_vertex_by_id(start_id)]
+        start = self.get_vertex_by_id(start_id)
+        end = self.get_vertex_by_id(end_id)
+
+        # Cidades a serem avaliadas
+        cidades_a_avaliar: Set[Vertex] = {start}
+        # Cidades já avaliadas
+        cidades_ja_avaliadas: Set[Vertex] = set()
+        came_from: Dict[Vertex, Vertex] = {}
+        # Distância do início até cada vértice
+        g_score: Dict[Vertex, float] = {vertex: float(
+            'inf') for vertex in self.vertices.values()}
+        # Distância do início até o início é 0
+        g_score[start] = 0
+        # Distância do início até o final passando por cada vértice
+        f_score: Dict[Vertex, float] = {vertex: float(
+            'inf') for vertex in self.vertices.values()}
+        # Distância em linha reta do vértice até o final
+        # (a solução ótima tenta minimizar até essa distância)
+        f_score[start] = start.get_euclidian_distance(end)
+        # Enquanto houver cidades a serem avaliadas
+        while cidades_a_avaliar:
+            # Define a cidade a ser avaliada como a cidade com menor distância até o final
+            onde_estou: Vertex = min(cidades_a_avaliar,
+                                     key=lambda vertex: f_score[vertex])
+            # Se chegou ao fim, constrói o caminho e o retorna
+            if onde_estou == end:
+                path = [onde_estou]
+                while onde_estou in came_from:
+                    onde_estou = came_from[onde_estou]
+                    path.append(onde_estou)
+                return path[::-1]
+            # Remove a cidade a ser avaliada do conjunto de cidades a serem
+            # avaliadas e adiciona ao conjunto de cidades já avaliadas
+            cidades_a_avaliar.remove(onde_estou)
+            cidades_ja_avaliadas.add(onde_estou)
+            # Para cada vizinho da cidade a ser avaliada
+            for neighbor in onde_estou.neighbors:
+                # Se o vizinho já foi avaliado, pula para o próximo
+                if neighbor in cidades_ja_avaliadas:
+                    continue
+                # Calcula a distância do início até o vizinho passando pela cidade a ser avaliada
+                tentative_g_score = g_score[onde_estou] + \
+                    onde_estou.get_euclidian_distance(neighbor)
+                # Se a cidade a ser avaliada não está no conjunto de cidades
+                # a serem avaliadas a adiciona
+                if neighbor not in cidades_a_avaliar:
+                    cidades_a_avaliar.add(neighbor)
+
+                # Se estiver, mas a distância do início até o vizinho passando
+                # pela cidade a ser avaliada for maior que a distância já
+                #  calculada, pula para o próximo
+                elif tentative_g_score >= g_score[neighbor]:
+                    continue
+                # Se chegou até aqui, é porque a distância do início até o
+                # vizinho passando pela cidade a ser avaliada é menor que a
+                # distância já calculada
+                came_from[neighbor] = onde_estou
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = g_score[neighbor] + \
+                    neighbor.get_euclidian_distance(end)
+                if view_trace:
+                    print(f"{onde_estou.name} -> {neighbor.name} \t\t\t "
+                          f"g({neighbor.name})={g_score[neighbor]}, "
+                          f"h({neighbor.name})={neighbor.get_euclidian_distance(end)}, "
+                          f"f({neighbor.name})={f_score[neighbor]}")
+        # Não há caminho entre o início e o fim
+        if view_trace:
+            print(f"Não há caminho entre {start.name} e {end.name}")
+        return []
---
+++ ia/search/main.py
@@ -0,0 +1,150 @@
+"""Main module
+
+"""
+#!/usr/bin/env python3
+import argparse
+import sys
+from typing import List
+from csv import reader as csv_reader
+from graph import Graph, Vertex
+
+
+def print_path(path: List[Vertex]):
+    """Printa o caminho a ser tomado e a distância total
+
+    Args:
+        path (List[Vertex]): Lista de nós que representam as cidades
+    """
+    if not path or len(path) < 1:
+        print("Não há caminho entre as cidades")
+        return
+    print("Caminho a ser tomado: " +
+          " -> ".join([vertex.name for vertex in path]))
+
+    print("Distância total: " +
+          str(sum([path[i].get_euclidian_distance(path[i+1]) for i in range(len(path)-1)]))+"km\n")
+
+
+def read_cities_from_file(file_path: str):
+    """lê o arquivo csv com as cidades, identificador e suas coordenadas\n
+    formato esperado do cabeçalho e das linhas do arquivo csv:\n
+    ID,Name,Latitude,Longitude\n
+    1,Uberlândia,-18.9128,-48.2755\n
+    2,Uberaba,-19.7474,-47.9381\n
+
+
+    Args:
+        file_path (str): caminho do arquivo csv 
+
+    Returns:
+        _type_: List[Tuple[int, str, float, float]
+    """
+    cities_info_parsed = []
+    try:
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            reader = csv_reader(file)
+            next(reader)
+            for row in reader:
+                cities_info_parsed.append(
+                    (int(row[0]), row[1], float(row[2]), float(row[3])))
+    except FileNotFoundError:
+        print(f"Arquivo {file_path} com os dados das cidades não encontrado")
+        sys.exit(1)
+    return cities_info_parsed
+
+
+def read_edges_from_file(file_path: str):
+    """lê o arquivo csv com as arestas entre as cidades\n
+    formato esperado do cabeçalho e das linhas do arquivo csv:\n
+    City1,City2\n
+    1,2\n
+    1,3\n
+
+    Args:
+        file_path (str): caminho do arquivo csv
+
+    Returns:
+        List[Tuple[int, int]: Lista de tuplas com as arestas entre as cidades
+    """
+    edges_parsed = []
+    try:
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            reader = csv_reader(file)
+            next(reader)  # Pula o cabeçalho
+            for row in reader:
+                city1_id = int(row[0])
+                city2_id = int(row[1])
+                edges_parsed.append((city1_id, city2_id))
+    except FileNotFoundError:
+        print(
+            f"Arquivo {file_path} com as arestas entre as cidades não encontrado")
+        sys.exit(1)
+    return edges_parsed
+
+
+def main(
+        starting_city: int,
+        view_trace: bool,
+        end_city: int,
+        cities_filepath: str,
+        edges_filepath: str
+):
+    """Função principal que lê os arquivos csv com as cidades e as arestas
+    entre elas, instância o grafo e executa o algoritmo A* para encontrar 
+    o menor caminho entre as cidades
+
+    Args:
+        starting_city (int): id da cidade de origem
+        view_trace (bool): indica se deve ser mostrada a árvore de vértices visitados
+        end_city (int): id da cidade de destino
+        cities_filepath (str): caminho do arquivo csv com a informação das 
+        cidades como id, nome e coordenadas
+        edges_filepath (str): caminho do arquivo csv com as arestas entre as cidades
+    """
+
+    # Instanciando o grafo
+    g = Graph()
+
+    # Lista de cidades com identificador, nome e coordenadas aproximadas (latitude, longitude)
+    cities_info = read_cities_from_file(cities_filepath)
+
+    # Adicionando vértices ao grafo
+    for city in cities_info:
+        g.add_vertex(Vertex(*city))
+
+    edges = read_edges_from_file(edges_filepath)
+
+    for edge in edges:
+        g.add_edge(*edge)
+    if end_city != -1:
+        print(
+            f"De {g.get_vertex(starting_city).name} para {g.get_vertex(end_city).name}")
+        path = g.a_star(1, end_city, view_trace=view_trace)
+        print_path(path)
+        return
+    for i in range(1, 11):
+        if i == starting_city:
+            continue
+        print(
+            f"De {g.get_vertex(starting_city).name} para {g.get_vertex_by_id(i).name}")
+        path = g.a_star(1, i, view_trace=view_trace)
+        print_path(path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("A* Search Algorithm")
+
+    parser.add_argument("-s", "--starting_city",
+                        help="Starting city", type=int, default=1)
+    parser.add_argument(
+        "--view_trace", help="View all vertex visited in the search", action="store_true")
+    parser.add_argument("-e", "--end_city",
+                        help="End city", type=int, default=-1)
+    parser.add_argument("-f", "--cities_filepath",
+                        help="Path to a csv file containing the cities data", default="cities.csv")
+    parser.add_argument("--edges_filepath", help="Path to a csv file containing the edges data",
+                        default="edges.csv")
+    args = parser.parse_args()
+
+    main(args.starting_city, args.view_trace,
+         args.end_city, args.cities_filepath, args.edges_filepath)
